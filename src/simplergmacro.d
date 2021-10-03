682db1fdba88013b63d3f6cc3210cd49b7d01fd0
Kai Gonzalez <gkai70264@gmail.com>
2021-10-03 17:54:48 -0400

    GMacro fix

--- src/simplergmacro.d
+++ src/simplergmacro.d
@@ -14,7 +14,7 @@ Copyright (C) 2021 Kai D. Gonzalez
  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  See the GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
+ You should have received a copy of the GNU General Public License along with GMacro. If not, see http://www.gnu.org/licenses/.
 
 */
 
